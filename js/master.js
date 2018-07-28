1992429e9ccf97f13ddedafdc6b5e7be772763c4
Lydzje <to.lydzje@gmail.com>
2018-07-28 17:59:46 +0200

    New decryption effect when visiting the page

--- js/master.js
+++ js/master.js
@@ -6,19 +6,132 @@ var body = document.body;
 var index = document.getElementById('index');
 var about = document.getElementById('about');
 var contact = document.getElementById('contact');
+var command = document.getElementById('command');
 var goto = document.getElementById('goto');
+
 // index objects
 var indexTitle = document.getElementById('index-title');
 var nav1 = document.getElementById('navigator1');
 var nav2 = document.getElementById('navigator2');
+
 // nav2 links
 var aboutLink = document.getElementById('about-link');
 var contactLink = document.getElementById('contact-link');
 var currentSection = "index";
 var initialFading = true;
 
+
 // INITIALIZATION
 // --------------
+setInterval(blink, 500);
+
+// Decryption effect code
+var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-=+<>,./?¿[{()}]!@#$%^&*~`\|".split('');
+
+// Title
+var titleTextElement = document.getElementById("index-title-text");
+var titleText = titleTextElement.innerHTML;
+var titleTextCopy = titleText;
+var titleLen = titleText.length;
+
+var titleObj = {
+    element  : titleTextElement,
+    text     : titleText,
+    textCopy : titleTextCopy,
+    len      : titleLen,
+    chrsSet  : 0,
+    mutTimer : null,
+    incTimer : null
+}
+
+titleObj.mutTimer = setInterval(
+    function() {
+	textMutation(titleObj);
+    }, 20
+);
+
+setTimeout(
+    function() {
+	titleObj.incTimer = setInterval(
+	    function() {
+		incrementChrsSet(titleObj)
+	    }, 50
+	);
+    }, 1500
+);
+// end Title
+
+// About button
+var aboutTextElement = document.getElementById("about-text");
+var aboutText = aboutTextElement.innerHTML;
+var aboutTextCopy = aboutText;
+var aboutLen = aboutText.length;
+
+var aboutObj = {
+    element  : aboutTextElement,
+    text     : aboutText,
+    textCopy : aboutTextCopy,
+    len      : aboutLen,
+    chrsSet  : 0,
+    mutTimer : null,
+    incTimer : null
+}
+
+aboutObj.mutTimer = setInterval(
+    function() {
+	textMutation(aboutObj);
+    }, 20
+);
+
+setTimeout(
+    function() {
+	aboutObj.incTimer = setInterval(
+	    function() {
+		incrementChrsSet(aboutObj)
+	    }, 50
+	);
+    }, 2000
+);
+// end About button
+
+// Contact button
+var contactTextElement = document.getElementById("contact-text");
+var contactText = contactTextElement.innerHTML;
+var contactTextCopy = contactText;
+var contactLen = contactText.length;
+
+var contactObj = {
+    element  : contactTextElement,
+    text     : contactText,
+    textCopy : contactTextCopy,
+    len      : contactLen,
+    chrsSet  : 0,
+    mutTimer : null,
+    incTimer : null
+}
+
+contactObj.mutTimer = setInterval(
+    function() {
+	textMutation(contactObj);
+    }, 20
+);
+
+setTimeout(
+    function() {
+	contactObj.incTimer = setInterval(
+	    function() {
+		incrementChrsSet(contactObj)
+	    }, 50
+	);
+    }, 2500
+);
+// end Contact button
+
+// end Decryption effect code
+
+
+// FUNCTIONS
+// ---------
 // Blinking text
 function blink(){
     if(goto.style.visibility=='hidden') {
@@ -27,15 +140,49 @@ function blink(){
         goto.style.visibility='hidden';
     }
 }
-setInterval(blink, 500);
 
-// FUNCTIONS
-// ---------
+
+function setCharAt(str, index, chr) {
+    if(index > str.length-1){
+	return str;
+    }
+
+    return str.substr(0, index) + chr + str.substr(index+1);
+}
+
+
+function incrementChrsSet(textObj) {
+    var len = textObj.len;
+
+    textObj.chrsSet += 1;
+    textObj.textCopy = setCharAt(textObj.textCopy, textObj.chrsSet-1, textObj.text.charAt(textObj.chrsSet-1));
+
+    if(textObj.chrsSet == len) {
+	clearInterval(textObj.mutTimer);
+	clearInterval(textObj.incTimer);
+	textObj.element.innerHTML = textObj.textCopy;
+    }
+}
+
+
+function textMutation(textObj) {
+    var len = textObj.len;
+
+    for(var i = textObj.chrsSet; i < len; i++) {
+	var random = Math.round(Math.random() * (characters.length-1));
+	var chr = characters[random];
+	textObj.textCopy = setCharAt(textObj.textCopy, i, chr);
+	textObj.element.innerHTML = textObj.textCopy;
+    }
+}
+
+
 function setGoto(here) {
   if (here != '') {  
     goto.innerHTML = here;
   } else goto.innerHTML = '...'
 }
+
 
 function moveTo(section) {
   if (initialFading) {
@@ -103,9 +250,11 @@ function moveTo(section) {
   }
 }
 
+
 function fadeIn() {
-  indexTitle.style.opacity = "1";
-  indexTitle.style.pointerEvents = "auto";
-  nav2.style.opacity = "1";
-  nav2.style.pointerEvents = "auto";
+    indexTitle.style.opacity = "1";
+    indexTitle.style.pointerEvents = "auto";
+    nav2.style.opacity = "1";
+    nav2.style.pointerEvents = "auto";
+    command.style.opacity = "1";
 }
